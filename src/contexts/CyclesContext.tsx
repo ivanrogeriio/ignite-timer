2eb739f8b3bfdcd0af86e98d93f92acb92dbf0af
Ivan Rogério <ivanpassosfilho@gmail.com>
2023-04-08 02:28:10 -0300

    Aula 42 - Salvando estado no storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
 import {
   ReactNode,
   createContext,
+  useEffect,
   useReducer,
   useState,
 } from 'react';
@@ -11,6 +12,7 @@ import {
   interruptCurrentCycleAction,
   markCurrentCycleFinishedAction,
 } from '../reducers/cycles/actions';
+import { differenceInSeconds } from 'date-fns';
 
 interface CreateCycleData {
   task: string;
@@ -48,19 +50,52 @@ interface CyclesContextProviderProps {
 export const CyclesContextProvider = ({
   children,
 }: CyclesContextProviderProps) => {
-  const [cycleState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cycleState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storageStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycle-state-1.0.0',
+      );
+
+      if (storageStateAsJSON) {
+        return JSON.parse(storageStateAsJSON);
+      }
+
+      return initialState;
+    },
+  );
 
   const { cycles, activeCycleId } = cycleState;
-
-  const [amountSecondPast, setAmmountSecondPast] =
-    useState(0);
 
   const activeCycle = cycles.find(
     (cycle: any) => cycle.id === activeCycleId,
   );
+
+  const [amountSecondPast, setAmmountSecondPast] = useState(
+    () => {
+      if (activeCycle) {
+        return differenceInSeconds(
+          new Date(),
+          new Date(activeCycle.startDate),
+        );
+      }
+
+      return 0;
+    },
+  );
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cycleState);
+
+    localStorage.setItem(
+      '@ignite-timer:cycle-state-1.0.0',
+      stateJSON,
+    );
+  }, [cycleState]);
 
   function sendSetAmmountSecondPastToHook(seconds: number) {
     setAmmountSecondPast(seconds);
